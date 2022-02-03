3b033ebef664cf7a7a0f0c19ea4b00dc5cf2b462
Qimon <simon.bergerard@gmail.com>
2022-02-03 12:03:14 +0100

    5 files modified

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # angular-tuto-qimon
 
 [See all Exercise here](https://angular.io/start)
+[See CLI documentation here](https://angular.io/cli)
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a97634-c3smaq)
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -7,9 +7,12 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
-  
   // 5. The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent
-  @Input() product!: Product;
+  // @Input() product!: Product;
+  @Input() product: Product | undefined;
+  // 2. The @Output() decorator allows the ProductAlertsComponent to emit an event when the value of the notify property changes
+  @Output() notify = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
